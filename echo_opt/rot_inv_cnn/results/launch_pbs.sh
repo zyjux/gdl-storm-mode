1d88f7ecd9e97fadbab69c4dc5b7d1410ddf2364
Lander <lander.verhoef@gmail.com>
2022-10-14 13:45:46 -0600

    Initial commit

---
+++ echo_opt/rot_inv_cnn/results/launch_pbs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#PBS -N rot_inv_echo
+#PBS -l select=1:ncpus=1:ngpus=1:mem=64GB
+#PBS -l walltime=24:00:00
+#PBS -l gpu_type=v100
+#PBS -A NAML0001
+#PBS -q casper
+#PBS -o out
+#PBS -e out
+source ~/.bashrc
+conda activate storm-mode
+echo-run hyperparameter.yaml conf.yaml -n $PBS_JOBID
